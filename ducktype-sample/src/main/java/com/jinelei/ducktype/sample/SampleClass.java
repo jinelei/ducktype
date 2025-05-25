4dc4671d47ba4a2e577353a8534da6705b004e1a
jinelei <jinelei@gmail.com>
2025-05-25 21:22:51 +0800

    添加测试类1

--- ducktype-sample/src/main/java/com/jinelei/ducktype/sample/SampleClass.java
+++ ducktype-sample/src/main/java/com/jinelei/ducktype/sample/SampleClass.java
@@ -2,27 +2,22 @@ package com.jinelei.ducktype.sample;
 
 import java.util.List;
 
-public class SampleClass implements SampleInterface {
-    @Override()
+public class SampleClass {
     public void methodVoid() {
     }
 
-    @Override()
     public int methodInt() {
         return 0;
     }
 
-    @Override()
     public String methodString() {
         return "";
     }
 
-    @Override()
     public List<String> methodList() {
         return List.of();
     }
 
-    @Override()
     public String[] methodArray() {
         return new String[0];
     }
